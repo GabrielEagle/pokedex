d39748b08193fc23bc69a3d8e006a541f8fdd32b
GabrielEagle <dialixkat@gmail.com>
2022-10-26 12:32:57 -0300

    error handling homepage
    error handling homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Input } from "./Home.styles"
 import CardPokemon, { CardPokemonProps } from "../components/CardPokemon";
-import { List }  from "../components/List.style";
+import { List } from "../components/List.style";
 
 import Navbar from "../components/Navbar";
 import Title from "../components/Title";
@@ -15,62 +15,69 @@ function Home() {
     const [pokemonList, setPokemonList] = useState<CardPokemonProps[]>([]);
     const [textoBusca, setTextoBusca] = useState("");
 
-    
+
 
     async function getPokemonData() {
         const { data } = await api.get("/pokemon?limit=1100");
 
-        const dadosCompletos = await Promise.all(
-            data.results.map(async (result: { url: string }) => {
-                const { data } = await api.get(result.url);
-  
-                  return {
-                      id: data.id,
-                      name: data.name,
-                      types: data.types,
-                  };
-              })
-        );
+        if (data) {
 
-        setPokemonList(
-           dadosCompletos
-        );
-        setIsLoading(false);
+            const dadosCompletos = await Promise.all(
+                data.results.map(async (result: { url: string }) => {
+                    const { data } = await api.get(result.url);
+
+                    return {
+                        id: data.id,
+                        name: data.name,
+                        types: data.types,
+                    };
+                })
+            );
+
+            setPokemonList(
+                dadosCompletos
+            );
+            setIsLoading(false);
+        }
+        else {
+         <p>Damn... These Pokeballs are hard to open. Bad Request :c</p>
+        }
+
     }
 
-    useEffect (() => {
+    useEffect(() => {
         getPokemonData();
-     }, [ ])
+    }, [])
 
-     if(isLoading) {
-        return <img src="https://i.pinimg.com/originals/8a/4a/72/8a4a7213b43f4ec4f99db406be655f9e.gif" className="loading"/>;
-     }
+    if (isLoading) {
+        return <img src="https://i.pinimg.com/originals/8a/4a/72/8a4a7213b43f4ec4f99db406be655f9e.gif" className="loading" />;
+    }
 
     return (
         <>
             <Navbar />
             <Title text="Find all pokemons in one place" />
 
-            <Input type="text" placeholder="Find Pokemon by name or ID" value={textoBusca} onChange={(event)=> setTextoBusca(event.target.value)} />
+            <Input type="text" placeholder="Find Pokemon by name or ID" value={textoBusca} onChange={(event) => setTextoBusca(event.target.value)} />
 
             <List>
                 {pokemonList
                     .filter(pokemon => pokemon.name.includes(textoBusca) || String(pokemon.id) === textoBusca)
                     .map((pokemon, index) => {
-                return (
-                <CardPokemon 
-                    key={index} 
-                    id={pokemon.id} 
-                    name={pokemon.name} 
-                    types={pokemon.types} />
-                );
-            })}
-            
+                        return (
+                            <CardPokemon
+                                key={index}
+                                id={pokemon.id}
+                                name={pokemon.name}
+                                types={pokemon.types} />
+                        );
+                    })}
+
             </List>
         </>
     )
- }
-   
+}
+
 
 
 
